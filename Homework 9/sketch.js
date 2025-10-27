f96a7f501cda9a232180b28283fc132972d900f1
elijahja04-collab <elijahja04@gmail.com>
2025-10-27 16:41:16 -0600

    hw9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(120,45,78);
+  fill(255,255,0);
+  circle(200,100,75);
+  rect(165,125,70,90);
+  point(215,100);
+  point(185,100);
+  line(185, 110 , 215, 110);
+  triangle(185, 125, 215, 125, 200, 140);
+  text('Eli Anderson',175, 250);
+
+
+}
